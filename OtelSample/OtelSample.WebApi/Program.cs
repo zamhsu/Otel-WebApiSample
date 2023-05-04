88afb5c7bb87345c812c9d56887d749ed6a46f2d
zamhsu <sam2009mn32@gmail.com>
2023-05-04 21:44:05 +0800

    加入OpenTelemetry tracing的設定

--- OtelSample/OtelSample.WebApi/Program.cs
+++ OtelSample/OtelSample.WebApi/Program.cs
@@ -1,3 +1,8 @@
+using System.Reflection;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,14 +12,56 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// 反射取得服務相關的類別庫名稱
+var serviceName = Assembly.GetEntryAssembly()?.GetName().Name;
+var serviceVersion = Assembly.GetEntryAssembly()?.GetName().Version?.ToString();
+
+var prefix = "OtelSample";
+
+var assemblies = AppDomain.CurrentDomain.GetAssemblies().Where(q => q.GetName().Name.StartsWith(prefix));
+var sources = assemblies.Select(q => q.GetName().Name);
+foreach (var source in sources)
+{
+    Console.WriteLine(source);
+}
+
+// tracing
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracerProviderBuilder =>
+        tracerProviderBuilder
+            .SetResourceBuilder(ResourceBuilder.CreateDefault()
+                .AddService(serviceName, serviceVersion: serviceVersion))
+            .AddSource(sources.ToArray())
+            .AddAspNetCoreInstrumentation(options =>
+            {
+                options.RecordException = true;
+            })
+            .AddHttpClientInstrumentation(options =>
+            {
+                options.RecordException = true;
+            })
+            .AddSqlClientInstrumentation(options =>
+            {
+                options.RecordException = true;
+                options.SetDbStatementForText = true;
+            })
+            .AddOtlpExporter(cfg =>
+            {
+                cfg.Endpoint = new Uri("http://localhost");
+                cfg.Protocol = OtlpExportProtocol.Grpc;
+            })
+            .AddConsoleExporter());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
